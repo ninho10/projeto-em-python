91d34f51951571b924c9bd6d3720fcb6d1cda55c
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-10-17 18:59:49 -0300

    Update JogoDaVida.py
    Estou atualizando o jogo com mas perguntas..

--- JogoDaVida.py
+++ JogoDaVida.py
@@ -1,4 +1,8 @@
 print('    ####   SEJA BEM VINDO AO JOGO DA VIDA   ####')
 print('')
-Nome = input(' Jogador Qual è sua Nome? ')
-Idade = input(' {} Qual é sua idade '.format(Nome))
+Nome = input(' Jogador Qual è sua Nome? --> ')
+AnoNasci = int(input(' Jogador {} que Ano você naisceu? --> '.format(Nome)))
+AnoAtual = int(input(' Jogador {} que Ano estamos ?--> '.format(Nome)))
+print('')
+Idade = AnoAtual - AnoNasci
+print('Jogador {}, Você tem {} ou vai fazer? '.format(Nome, Idade))
