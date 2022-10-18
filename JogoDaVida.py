110147f13928361364ce22b9886e2b3595c3c529
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-10-18 18:43:05 -0300

    Update JogoDaVida.py
    coloque uma condição no meu projeto.

--- JogoDaVida.py
+++ JogoDaVida.py
@@ -5,4 +5,10 @@ AnoNasci = int(input(' Jogador {} que Ano você naisceu? --> '.format(Nome)))
 AnoAtual = int(input(' Jogador {} que Ano estamos ?--> '.format(Nome)))
 print('')
 Idade = AnoAtual - AnoNasci
-print('Jogador {}, Você tem {} ou vai fazer? '.format(Nome, Idade))
+print('Jogador {}, Você tem {} anos ou vai fazer? '.format(Nome, Idade))
+N1 = int(input('se você tem, digite [1] se não digite [2]'))
+
+if N1 == 1:
+    print(' Como pode fazer uma festa e não me chamar, estou triste com Você {}.'.format(Nome))
+else:
+    print('{} estou feliz esse ano tem uma festa para eu ir rsrs. '.format(Nome))
