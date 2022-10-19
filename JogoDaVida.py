12fc16afe86272345385ef408cc795e4a4a875db
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-10-19 18:42:33 -0300

    Update JogoDaVida.py
    botei uma função e um loop

--- JogoDaVida.py
+++ JogoDaVida.py
@@ -1,3 +1,13 @@
+
+
+def ZeroVinte():
+    NumVin = 1
+    Cont = 0
+    while NumVin != 11:
+        NumVin = int(input('digite o numero que estou pensando : '))
+        Cont = Cont + 1
+
+
 print('    ####   SEJA BEM VINDO AO JOGO DA VIDA   ####')
 print('')
 Nome = input(' Jogador Qual è sua Nome? --> ')
@@ -12,3 +22,7 @@ if N1 == 1:
     print(' Como pode fazer uma festa e não me chamar, estou triste com Você {}.'.format(Nome))
 else:
     print('{} estou feliz esse ano tem uma festa para eu ir rsrs. '.format(Nome))
+print('')
+print(' {} vou te fazer um desafio vou pensar em um numero entre 0 e 20 '.format(Nome))
+ZeroVinte()
+print(' Parabens  o numero que eu pensei foi o 11 ')
