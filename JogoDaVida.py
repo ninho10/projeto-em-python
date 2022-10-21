70bd4cc54f0c2b806eb246854cc7446b705c5703
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-10-21 16:36:04 -0300

    Update JogoDaVida.py
    mudei muitas coisas no jogo e aumentei bastante ele, mas perguntas.

--- JogoDaVida.py
+++ JogoDaVida.py
@@ -8,6 +8,17 @@ def ZeroVinte():
         Cont = Cont + 1
 
 
+def chefe1():
+    RespChe1 = 1
+
+    while RespChe1 != 3:
+        RespChe1 = int(input('[1] Vasco   [2] Palmeiras   [3]Flamengo --> '))
+        if RespChe1 == 1:
+            print('voce errou seu ataque, tente de novo')
+        else:
+            print('voce acabou de levar um soco assim vai perde, tente de novo')
+
+
 print('    ####   SEJA BEM VINDO AO JOGO DA VIDA   ####')
 print('')
 Nome = input(' Jogador Qual è sua Nome? --> ')
@@ -16,13 +27,26 @@ AnoAtual = int(input(' Jogador {} que Ano estamos ?--> '.format(Nome)))
 print('')
 Idade = AnoAtual - AnoNasci
 print('Jogador {}, Você tem {} anos ou vai fazer? '.format(Nome, Idade))
-N1 = int(input('se você tem, digite [1] se não digite [2]'))
+N1 = int(input('se você tem, digite [1] se não digite [2] --> '))
 
 if N1 == 1:
-    print(' Como pode fazer uma festa e não me chamar, estou triste com Você {}.'.format(Nome))
+    print('Como pode fazer uma festa e não me chamar, estou triste com Você {}.'.format(Nome))
+    print('Com isso você perde um vida')
 else:
     print('{} estou feliz esse ano tem uma festa para eu ir rsrs. '.format(Nome))
+    print('me chamando para festa você ganha uma vida')
 print('')
-print(' {} vou te fazer um desafio vou pensar em um numero entre 0 e 20 '.format(Nome))
+print('{} vou te fazer um desafio vou pensar em um numero entre 0 e 20 '.format(Nome))
 ZeroVinte()
-print(' Parabens  o numero que eu pensei foi o 11 ')
+print('Parabéns o numero que eu pensei foi o 11 ')
+print('')
+print('Parabéns jogador {} chegamos no chefe '.format(Nome))
+print('vou te fazer uma pergunta acertando você passa de nivel.')
+print('')
+print('Qual é a maior torcida de futbol do Brasil')
+chefe1()
+print('nossa você atacou o chefe com todo o seu poder........')
+print('')
+print('Parabéns {}, você acabou de derrotar o chefe.......'.format(Nome))
+print('#### você passsou para um novo nivel ####')
+print('')
