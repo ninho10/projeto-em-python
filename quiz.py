c966d7a73d17db81482ca7dd899a6561e57daa7f
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-10-25 15:30:22 -0300

    Update quiz.py
    dei uma grande melhorada um jogo, tem mas perguntas e limpa a tela de uma pergunta para outra...

--- quiz.py
+++ quiz.py
@@ -1,12 +1,18 @@
+import os
+pontoUm = 0
+
+
 # MEU QUIZ EM PYTHON
 
+
 print(" Quiz do Paulo ")
-answer_user = input("você que jogar ?  (S/N) ")
+answer_user = input("você que jogar ?  (S/N) -- > ")
 print(answer_user)
 
 
 if answer_user != "S":
     quit()     # para termina o porgrama
+os.system('cls')
 
 print(" Começando o jogo.....")
 print("Qual e meu (nome)? \n (A) Pedro \n (B) Paulo \n (C) ninho \n ")
@@ -14,13 +20,50 @@ resposta1 = input(" Resposta : ")
 
 if resposta1 == "B":
     print("Parabens Correto")
+    pontoUm = pontoUm + 2
 else:
     print("Esta Errada !")
+os.system('cls')
 
 print("Qual e minha (idade)? \n (A) 35 \n (B) 30 \n (C) 34 \n ")
 resposta2 = input(" Resposta : ")
 
 if resposta2 == "C":
     print("Parabens Correto")
+    pontoUm = pontoUm + 2
 else:
     print("Esta Errada !")
+os.system('cls')
+
+print("Qual Bairro eu (moro)? \n (A) Campinho \n (B) Madureira \n (C) Vila Valqueire \n ")
+resposta2 = input(" Resposta : ")
+
+if resposta2 == "A":
+    print("Parabens Correto")
+    pontoUm = pontoUm + 2
+else:
+    print("Esta Errada !")
+os.system('cls')
+
+print("Qual o Nome do meu (filho)? \n (A) Pedro \n (B) Rafael \n (C) Davi \n ")
+resposta2 = input(" Resposta : ")
+
+if resposta2 == "C":
+    print("Parabens Correto")
+    pontoUm = pontoUm + 2
+else:
+    print("Esta Errada !")
+os.system('cls')
+
+print("Qual e meu (time)? \n (A) Vasco \n (B) Flamengo \n (C) Palmeiras \n ")
+resposta2 = input(" Resposta : ")
+
+if resposta2 == "B":
+    print("Parabens Correto")
+    pontoUm = pontoUm + 2
+else:
+    print("Esta Errada !")
+os.system('cls')
+
+print(f'Parabens Você terminou o Jogo total de pontos : {pontoUm}')
+print('FIM DE JOGO .....')
