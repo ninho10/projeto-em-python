46cd66ea6314df00c66266d138186b7c729f0c23
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-12-23 17:11:02 -0300

    Update carteiraDigital.py
    coloquei import, agora o programa esta conectado direto com o banco de dados.

--- carteiraDigital.py
+++ carteiraDigital.py
@@ -1,20 +1,38 @@
+import mysql.connector
+
+
 class Carteira:
-    def __init__(self, nome, banco, carteira):
+    def __init__(self, nome, banco, dinheiro):
         self.nome = nome
         self.banco = banco
-        self.carteira = carteira
+        self.dinheiro = dinheiro
 
 
 n1 = None
 n1 = input('Seu nome --> ')
-b1 = float(input('Qual e seu saldo bancario ? --> '))
-c1 = float(input('Quanto tem na sua carteira ? --> '))
+b1 = input('Qual é banco ? --> ')
+c1 = input(f'Quanto tem de dinheiro no {b1} ? --> ')
 
 carteira1 = Carteira(n1, b1, c1)
 
-total = (carteira1.banco + carteira1.carteira)
 
 print(f'{carteira1.nome} seja bem vido a carteira digital ')
-print(f'seu saldo no banco e de {carteira1.banco}')
-print(f' seu saldo na carteira e de {carteira1.carteira} ')
-print(f'{carteira1.nome} seu valor total em dinheiro e {total}')
+print(
+    f'seu banco é o {carteira1.banco} você tem {carteira1.dinheiro} reais na sua conta ')
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="190548",
+    database="mybanco"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO usuario (name, banco, valor, id) VALUES (%s, %s, %s, 0)"
+val = (carteira1.nome, carteira1.banco, carteira1.dinheiro)
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
