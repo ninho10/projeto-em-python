f3d84503e3cf35703357de2ad56e28baec500f4d
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-12-28 21:47:57 -0300

    Update pix.py
    agora o programa pode receber pix.

--- pix.py
+++ pix.py
@@ -1,15 +1,24 @@
 class Banco:
-    def __init__(self, nome, saldo):
+    def __init__(self, nome, banco, conta, saldo):
         self.nome = nome
+        self.banco = banco
+        self.conta = conta
         self.saldo = saldo
 
     def mandouPix(self, valor):
         return self.saldo - valor
 
+    def receberPix(self, valor):
+        return self.saldo + valor
 
-banco1 = Banco('Paulo', 2000)
-banco2 = Banco('rafael', 3000)
+
+banco1 = Banco('Paulo Roberto', 'banco do Brasil', '111', 2500)
+banco2 = Banco('Rafael Pereira', 'banco Itau', '112', 3000)
+banco3 = Banco('Monica Santos', 'banco do Bradesco', '113', 5000)
 
 n1 = float(input('Qual valor do pix você quer mandar -->'))
 print(f'O cliente {banco1.nome} tinha no seu saldo --> {banco1.saldo}')
 print(f'Mandou um Pix de {n1} agora seu saldo é --> {banco1.mandouPix(n1)} ')
+
+
+print(banco3.receberPix(1000))
