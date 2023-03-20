19723b57ec4a17889afd5cf3d8dd66ac20139ed7
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-03-20 10:38:48 -0300

    projeto escola
    primeiro commit, vai ser um projeto para  os professores da as notas dos aulos onde os alunos   terram um acesso a suas notas vai ter orientação a objeto e banco de dados

--- nota_aluno/index.py
+++ nota_aluno/index.py
@@ -1,1 +1,50 @@
-print('Ola mundo!')
+import os
+
+
+def nomeEscola():
+    print('  # ESCOLA XPTO #')
+
+
+def menuPrincipal():
+    print('''
+               MENU:
+
+               [1] - Ària de Professor
+               [2] - Ària de alunos
+               [3] - Sair
+
+             ===================================''')
+
+
+def areaProfessoar():
+    print('''
+               MENU:
+
+               [1] - Ària de Professor
+               [2] - Ària de alunos
+               [3] - Sair
+
+             ===================================''')
+
+
+while True:
+    os.system('cls')
+    menuPrincipal()
+    escolhaMenur = input('Escolha uma opeção ? --> ')
+    if (escolhaMenur == '1'):
+        os.system('cls')
+        nomeEscola()
+        print(' ÁRIA DO PROFESSOR')
+        loginProfessor = input('Login --> ')
+        senhaProfessor = input('Senha --> ')
+        teste = input('..............')
+
+    elif (escolhaMenur == '2'):
+        os.system('cls')
+        print('Ària de alunos')
+
+    else:
+        os.system('cls')
+        nomeEscola()
+        print('Fim...')
+        break
